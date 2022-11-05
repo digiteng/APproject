7612e66f86a5280673abbfb88b415c20fa4655ab
digiteng <digiteng@gmail.com>
2022-11-05 20:43:34 +0300

    Update ap_installer.sh

--- ap_installer.sh
+++ ap_installer.sh
@@ -10,7 +10,9 @@ sleep 1
 wget github.com/digiteng/APproject/releases/latest/download/ap.tar.gz -P /tmp
 echo -e "\e[32mNew Version Downloaded\e[0m"
 sleep 1
-openssl enc -d -aes256 -in /tmp/ap.tar.gz | tar xz -C /
+if [ -f /tmp/ap.tar.gz ]; then
+	openssl enc -d -aes256 -in /tmp/ap.tar.gz | tar xz -C /
+fi
 echo -e "\e[32mNew Version Installing...\e[0m"
 if [ -f /tmp/ap.tar.gz ]; then
 	tar -xzf /tmp/ap.tar.gz -C /tmp
@@ -43,7 +45,7 @@ if [ $OS = "Dream" ]; then
 		echo ""
 	else
 		echo " Downloading gstreamer1.0-plugins-good-mpg123 ......"
-		apt-get install install gstreamer1.0-plugins-good-mpg123 -y;
+		apt-get install gstreamer1.0-plugins-good-mpg123 -y;
 	fi	
 	if grep 'gstreamer1.0-plugins-good-equalizer' $ipath; then
 		echo ""
@@ -58,39 +60,39 @@ else
 		echo ""
 	else
 		echo " Downloading gstreamer1.0-plugins-base-volume ......"
-		opkg install install gstreamer1.0-plugins-base-volume -y;
+		opkg install gstreamer1.0-plugins-base-volume -y;
 	fi
 	if grep 'gstreamer1.0-plugins-good-ossaudio' $ipath; then
 		echo ""
 	else
 		echo " Downloading gstreamer1.0-plugins-good-ossaudio ......"
-		opkg install install gstreamer1.0-plugins-good-ossaudio -y;
+		opkg install gstreamer1.0-plugins-good-ossaudio -y;
 	fi
 	if grep 'gstreamer1.0-plugins-good-mpg123' $ipath; then
 		echo ""
 	else
 		echo " Downloading gstreamer1.0-plugins-good-mpg123 ......"
-		opkg install install install gstreamer1.0-plugins-good-mpg123 -y;
+		opkg install gstreamer1.0-plugins-good-mpg123 -y;
 	fi	
 	if grep 'gstreamer1.0-plugins-good-equalizer' $ipath; then
 		echo ""
 	else
 		echo " Downloading gstreamer1.0-plugins-good-equalizer ......"
-		opkg install install gstreamer1.0-plugins-good-equalizer -y;
+		opkg install gstreamer1.0-plugins-good-equalizer -y;
 	fi
 fi
 sleep 3
-if [ -d /usr/lib/enigma2/python/Plugins/Extensions/ap ]; then
-	echo -e "\e[32mNew Version Installed\e[0m"
-	rm -rf /tmp/ap.tar.gz
-	sleep 2
-	echo -e "\e[32mRestarting Enigma2 Gui...\e[0m"
-	if [ $OS = "Dream" ]; then
-		systemctl restart enigma2
-	else
-		killall -9 enigma2
-	fi
-else
-	echo -e "\e[31mNew Version Failed To Load\e[0m"
-fi
+# if [ -d /usr/lib/enigma2/python/Plugins/Extensions/ap ]; then
+	# echo -e "\e[32mNew Version Installed\e[0m"
+	# rm -rf /tmp/ap.tar.gz
+	# sleep 2
+	# echo -e "\e[32mRestarting Enigma2 Gui...\e[0m"
+	# if [ $OS = "Dream" ]; then
+		# systemctl restart enigma2
+	# else
+		# killall -9 enigma2
+	# fi
+# else
+	# echo -e "\e[31mNew Version Failed To Load\e[0m"
+# fi
 exit 0
