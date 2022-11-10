55c803e84cb6a931c7fc254ef7b8d0b076e98a82
digiteng <digiteng@gmail.com>
2022-11-10 20:04:29 +0300

    Update ap_installer.sh

--- ap_installer.sh
+++ ap_installer.sh
@@ -4,21 +4,21 @@ echo ""
 sleep 1
 echo -e "\e[32mOld Version Deleting...\e[0m"
 rm -rf /usr/lib/enigma2/python/Plugins/Extensions/AudioPlus
-rm -rf /tmp/ap.tar.gz
+rm -rf /ap.tar.gz
 echo -e "\e[32mOld Version Deleted\e[0m"
 sleep 1
-wget -q github.com/digiteng/APproject/releases/latest/download/ap.tar.gz -P /tmp
+wget -q github.com/digiteng/APproject/releases/latest/download/ap.tar.gz -P /
 sleep 5
 echo -e "\e[32mNew Version Downloaded\e[0m"
 sleep 1
 echo -e "\e[32mNew Version Installing...\e[0m"
-if [ -f /tmp/ap.tar.gz ]; then
-	openssl enc -d -aes256 -in /tmp/ap.tar.gz | tar xz -C /tmp
+if [ -f /ap.tar.gz ]; then
+	openssl enc -d -aes256 -in /ap.tar.gz | tar xz -C /tmp
 fi
 sleep 3
-if [ -d /tmp/tmp/ap ]; then
-	mv -v /tmp/tmp/ap/asound.conf /etc
-	mv -v /tmp/tmp/ap/AudioPlus /usr/lib/enigma2/python/Plugins/Extensions/
+if [ -d /ap ]; then
+	mv -v /tmp/ap/asound.conf /etc
+	mv -v /tmp/ap/AudioPlus /usr/lib/enigma2/python/Plugins/Extensions/
 fi
 sleep 3
 echo -e "\e[32mNew Version Installed...\e[0m"
@@ -30,66 +30,67 @@ else
 	ipath="/var/lib/opkg/status"
 fi
 echo""
-if [ $OS = "Dream" ]; then
-	echo -e "\e[32mUpdating...\e[0m"
-	apt-get update
-	if grep 'gstreamer1.0-plugins-base-volume' $ipath; then
-		echo ""
-	else
-		echo " Downloading gstreamer1.0-plugins-base-volume ......"
-		apt-get install gstreamer1.0-plugins-base-volume -y;
-	fi
-	if grep 'gstreamer1.0-plugins-good-ossaudio' $ipath; then
-		echo ""
-	else
-		echo " Downloading gstreamer1.0-plugins-good-ossaudio ......"
-		apt-get install gstreamer1.0-plugins-good-ossaudio -y;
-	fi
-	if grep 'gstreamer1.0-plugins-good-mpg123' $ipath; then
-		echo ""
-	else
-		echo " Downloading gstreamer1.0-plugins-good-mpg123 ......"
-		apt-get install gstreamer1.0-plugins-good-mpg123 -y;
-	fi	
-	if grep 'gstreamer1.0-plugins-good-equalizer' $ipath; then
-		echo ""
-	else
-		echo " Downloading gstreamer1.0-plugins-good-equalizer ......"
-		apt-get install gstreamer1.0-plugins-good-equalizer -y;
-	fi		
-else
-	echo -e "\e[32mUpdating...\e[0m"
-	opkg update
-	if grep 'gstreamer1.0-plugins-base-volume' $ipath; then
-		echo ""
-	else
-		echo " Downloading gstreamer1.0-plugins-base-volume ......"
-		opkg install gstreamer1.0-plugins-base-volume -y;
-	fi
-	if grep 'gstreamer1.0-plugins-good-ossaudio' $ipath; then
-		echo ""
-	else
-		echo " Downloading gstreamer1.0-plugins-good-ossaudio ......"
-		opkg install gstreamer1.0-plugins-good-ossaudio -y;
-	fi
-	if grep 'gstreamer1.0-plugins-good-mpg123' $ipath; then
-		echo ""
-	else
-		echo " Downloading gstreamer1.0-plugins-good-mpg123 ......"
-		opkg install gstreamer1.0-plugins-good-mpg123 -y;
-	fi	
-	if grep 'gstreamer1.0-plugins-good-equalizer' $ipath; then
-		echo ""
-	else
-		echo " Downloading gstreamer1.0-plugins-good-equalizer ......"
-		opkg install gstreamer1.0-plugins-good-equalizer -y;
-	fi
-fi
-sleep 3
-if [ -d /usr/lib/enigma2/python/Plugins/Extensions/AudioPlus ]; then
+if [ -f /usr/lib/enigma2/python/Plugins/Extensions/AudioPlus/plugin.py ]; then
 	echo -e "\e[32mNew Version Installed\e[0m"
-	rm -rf /tmp/ap.tar.gz
-	rm -rf /tmp/tmp
+	
+	if [ $OS = "Dream" ]; then
+		echo -e "\e[32mUpdating...\e[0m"
+		apt-get update
+		if grep 'gstreamer1.0-plugins-base-volume' $ipath; then
+			echo ""
+		else
+			echo " Downloading gstreamer1.0-plugins-base-volume ......"
+			apt-get install gstreamer1.0-plugins-base-volume -y;
+		fi
+		if grep 'gstreamer1.0-plugins-good-ossaudio' $ipath; then
+			echo ""
+		else
+			echo " Downloading gstreamer1.0-plugins-good-ossaudio ......"
+			apt-get install gstreamer1.0-plugins-good-ossaudio -y;
+		fi
+		if grep 'gstreamer1.0-plugins-good-mpg123' $ipath; then
+			echo ""
+		else
+			echo " Downloading gstreamer1.0-plugins-good-mpg123 ......"
+			apt-get install gstreamer1.0-plugins-good-mpg123 -y;
+		fi	
+		if grep 'gstreamer1.0-plugins-good-equalizer' $ipath; then
+			echo ""
+		else
+			echo " Downloading gstreamer1.0-plugins-good-equalizer ......"
+			apt-get install gstreamer1.0-plugins-good-equalizer -y;
+		fi		
+	else
+		echo -e "\e[32mUpdating...\e[0m"
+		opkg update
+		if grep 'gstreamer1.0-plugins-base-volume' $ipath; then
+			echo ""
+		else
+			echo " Downloading gstreamer1.0-plugins-base-volume ......"
+			opkg install gstreamer1.0-plugins-base-volume -y;
+		fi
+		if grep 'gstreamer1.0-plugins-good-ossaudio' $ipath; then
+			echo ""
+		else
+			echo " Downloading gstreamer1.0-plugins-good-ossaudio ......"
+			opkg install gstreamer1.0-plugins-good-ossaudio -y;
+		fi
+		if grep 'gstreamer1.0-plugins-good-mpg123' $ipath; then
+			echo ""
+		else
+			echo " Downloading gstreamer1.0-plugins-good-mpg123 ......"
+			opkg install gstreamer1.0-plugins-good-mpg123 -y;
+		fi	
+		if grep 'gstreamer1.0-plugins-good-equalizer' $ipath; then
+			echo ""
+		else
+			echo " Downloading gstreamer1.0-plugins-good-equalizer ......"
+			opkg install gstreamer1.0-plugins-good-equalizer -y;
+		fi
+	fi
+	sleep 3
+	rm -rf /ap.tar.gz
+	rm -rf /tmp/ap
 	sleep 2
 	echo -e "\e[1;33mRestarting Enigma2 Gui...\e[0m"
 	if [ $OS = "Dream" ]; then
